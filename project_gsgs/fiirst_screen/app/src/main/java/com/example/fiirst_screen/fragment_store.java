0c90ff0d4823aba6bbdb8a144561f6c706e4b7e9
LeeJJ07 <jkj9513@naver.com>
2023-09-19 23:22:50 +0900

    학생 회원가입 안드로이드 스튜디오와 mysql을 php+apache 서버를 통해 연동 성공

--- project_gsgs/fiirst_screen/app/src/main/java/com/example/fiirst_screen/fragment_store.java
+++ project_gsgs/fiirst_screen/app/src/main/java/com/example/fiirst_screen/fragment_store.java
@@ -19,6 +19,8 @@ public class fragment_store extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
         return inflater.inflate(R.layout.fragment_store,container,false);
     }
 }
