60099503041d6dbda6fcb1786df99811b4451fc5
KimJaHyeon <144971739+KimJaHyeon@users.noreply.github.com>
2023-09-24 00:26:22 +0900

    홈화면 음식 버튼

--- project_gsgs/fiirst_screen/app/src/main/java/com/example/fiirst_screen/Frag_home.java
+++ project_gsgs/fiirst_screen/app/src/main/java/com/example/fiirst_screen/Frag_home.java
@@ -1,13 +1,20 @@
 package com.example.fiirst_screen;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.viewmodel.CreationExtras;
 
 public class Frag_home extends Fragment {
-    @NonNull
     @Override
-    public CreationExtras getDefaultViewModelCreationExtras() {
-        return super.getDefaultViewModelCreationExtras();
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
     }
 }
