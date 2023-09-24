1eb98fd10c56caf975fdf289b2184f1c75ee3ee7
KimJaHyeon <144971739+KimJaHyeon@users.noreply.github.com>
2023-09-24 17:09:57 +0900

    홈화면 검색까지(첫페이지 제대로 바꿔놈)

--- project_gsgs/fiirst_screen/app/src/main/java/com/example/fiirst_screen/MainActivity.java
+++ project_gsgs/fiirst_screen/app/src/main/java/com/example/fiirst_screen/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         tv_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),user_home.class);
+                Intent intent = new Intent(getApplicationContext(),log_in.class);
                 startActivity(intent);
             }
         });
